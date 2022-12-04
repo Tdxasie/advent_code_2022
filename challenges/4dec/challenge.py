b625e46dff9a8c20fdbd9fb66a575d4eb5ae6500
Tdxasie <pierre.garnier1618@gmail.com>
2022-12-04 11:07:29 +0100

    found a solution with less characters

--- challenges/4dec/challenge.py
+++ challenges/4dec/challenge.py
@@ -21,7 +21,7 @@ print(sum([int(x[0][0]<=x[1][0]<=x[0][1] and x[0][0]<=x[1][1]<=x[0][1] or x[1][0
 print(sum([int(x[0]<=y[0]<=x[1]and x[0]<=y[1]<=x[1]or y[0]<=x[0]<=y[1]and y[0]<=x[1]<=y[1])for x,y in [[list(map(int,i.split('-')))for i in l.split(',')]for l in open('v').read().splitlines()]]))
 
 # chal1 
-print(sum([int(a<=c<=b and a<=d<=b or c<=a<=d and c<=b<=d)for (a,b),(c,d)in [[list(map(int,i.split('-')))for i in l.split(',')]for l in open('v').read().splitlines()]]))
+print(sum([a<=c<=b and a<=d<=b or c<=a<=d and c<=b<=d for (a,b),(c,d)in [[list(map(int,i.split('-')))for i in l.split(',')]for l in open('v').read().splitlines()]]))
 
 # chal2
-print(sum([int(a<=c<=b or a<=d<=b or c<=a<=d or c<=b<=d)for (a,b),(c,d)in [[list(map(int,i.split('-')))for i in l.split(',')]for l in open('v').read().splitlines()]]))
+print(sum([a<=c<=b or a<=d<=b or c<=a<=d or c<=b<=d for (a,b),(c,d)in [[list(map(int,i.split('-')))for i in l.split(',')]for l in open('v').read().splitlines()]]))
