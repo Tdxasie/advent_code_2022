1575cd2fd1afabde4f720e361807ad0d62c71e5d
pierre.garnier <pierre.garnier@scalian.com>
2022-12-09 23:02:50 +0100

    chal1 done chal2 WIP

--- challenges/9dec/challenge2.py
+++ challenges/9dec/challenge2.py
@@ -1,5 +1,6 @@
 from operator import add
-# start in (0,0)
+
+import matplotlib.pyplot as plt
 
 directions = {
               'L' : (-1, 0),
@@ -7,6 +8,7 @@ directions = {
               'D' : (0, -1),
               'R' : (1, 0),
              }
+
 
 class Head:
     def __init__(self, pos: tuple):
@@ -25,10 +27,13 @@ class Head:
         return tail_positions
             
 class Tail:
-    def __init__(self, head, pos: tuple, tail = None):
+    def __init__(self, head, pos: tuple):
         self.head = head
-        self.tail = tail
         self.pos = pos
+        self.tail = None
+    
+    def add_tail(self, tail):
+        self.tail = tail
     
     def follow(self, target_pos: tuple):
         # check if target pos close
@@ -37,16 +42,16 @@ class Tail:
         disty = target_pos[1] - self.pos[1]
         
         if abs(distx) <= 1 and abs(disty) <= 1:
-            return tuple(self.pos)
+            if self.tail:
+                return self.tail.follow(self.pos)
         
         distx = distx/abs(distx) if distx != 0 else 0
         disty = disty/abs(disty) if disty != 0 else 0
         
-        
         self.pos = list(map(add, self.pos, (distx, disty)))
         
         if self.tail:
-            self.tail.follow(self.pos)
+            return self.tail.follow(self.pos)
         
         return tuple(self.pos)
 
@@ -60,8 +65,15 @@ def run_sim(filename):
     lines = parse_file(filename)
     
     head = Head([0, 0])
-    tail = Tail(head, [0,0])
-    head.add_tail(tail)
+    tail1 = Tail(head, [0,0])
+    head.add_tail(tail1)
+    
+    tails = [tail1]
+    
+    for i in range(8):
+        tail = Tail(tails[i], [0,0])
+        tails[i].add_tail(tail)
+        tails.append(tail)
 
     tail_positions = []
     
@@ -70,12 +82,14 @@ def run_sim(filename):
         distance = int(distance)
         direction = directions[command]
         
-        
         new_positions = head.move(direction, distance)
         
         tail_positions.extend(new_positions)
     
     print(len(set(tail_positions)))
+    plt.plot([i[0] for i in tail_positions], [i[1] for i in tail_positions], '*')
+    plt.grid(1)
+    plt.show()
     
 if __name__ == '__main__':
-    run_sim('9dec/input.txt')
+    run_sim('9dec/ex2.txt')
